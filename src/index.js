4dc03ecb2af946368d592f4dca5a6e2268c72b5c
jiangwei <jiangwei@chelun.com>
2020-08-03 14:18:58 +0800

    update package.json

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import Button from './comp/button';
+
+export default { 
+    Button
+};
+// console.log(ENV)
+// ReactDOM.render(<Button>{ENV}</Button>,document.getElementById('app'));
